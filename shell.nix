2893894e6f6e4c7e9ea6919f6b2cd239ff7a651f
elcuervo <elcuervo@elcuervo.net>
2022-12-08 14:38:27 -0300

    Adds heroku

--- shell.nix
+++ shell.nix
@@ -1,9 +1,11 @@
 let
   sources = import ./nix/sources.nix;
   nixpkgs = import sources.nixpkgs { };
-  mkBundlerAppDevShell = nixpkgs.callPackage (import sources.bundler-app-dev-shell) {};
-in mkBundlerAppDevShell {
+  mkBundlerAppDevShell = nixpkgs.callPackage (import sources.bundler-app-dev-shell) { };
+in
+mkBundlerAppDevShell {
   buildInputs = with nixpkgs; [
     ruby_3_1
+    heroku
   ];
 }
